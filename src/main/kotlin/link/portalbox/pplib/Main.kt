88a48eb04c8c297d781b9ca0d84b573934e4f88e
AkkihBR <contact@akkih.com>
2023-04-09 15:02:38 -0300

    refactor(main): clean up main file

--- src/main/kotlin/link/portalbox/pplib/Main.kt
+++ src/main/kotlin/link/portalbox/pplib/Main.kt
@@ -1,18 +1,3 @@
 package link.portalbox.pplib
 
-import link.portalbox.pplib.manager.MarketplacePluginManager
-import link.portalbox.pplib.service.SpigotMCService
-import link.portalbox.pplib.type.MarketplaceService
-
-fun main(args: Array<String>) {
-    // PluginPortal: 108700
-    // Oraxen: 72448
-    // EssentialsX: 9089
-    // Vault: 34315
-    // WorldEdit: 31034
-    // KoChat: 107564
-
-    // Test, delete after
-    MarketplacePluginManager.registerService(MarketplaceService.SPIGOTMC, SpigotMCService())
-    println(MarketplacePluginManager.getPlugin(MarketplaceService.SPIGOTMC, 107564).downloadURL)
-}
+fun main(args: Array<String>) {}
