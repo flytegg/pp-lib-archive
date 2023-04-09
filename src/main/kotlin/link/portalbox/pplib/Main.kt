15170b4855b11ad454c9f0d1d8ca8bf74d9f80cc
AkkihBR <contact@akkih.com>
2023-04-09 10:46:35 -0700

    fly caused issues, workign ow

--- src/main/kotlin/link/portalbox/pplib/Main.kt
+++ src/main/kotlin/link/portalbox/pplib/Main.kt
@@ -1,3 +1,13 @@
 package link.portalbox.pplib
 
-fun main(args: Array<String>) {}
+import link.portalbox.pplib.manager.MarketplacePluginManager
+import link.portalbox.pplib.manager.MarketplacePluginManager.loadIndex
+import link.portalbox.pplib.service.SpigotMCService
+import link.portalbox.pplib.type.MarketplaceService
+
+fun main(args: Array<String>) {
+    MarketplacePluginManager.registerService(MarketplaceService.SPIGOTMC, SpigotMCService())
+    loadIndex()
+    val plugin = MarketplacePluginManager.getPlugin(MarketplaceService.SPIGOTMC, 91557)
+    println(plugin)
+}
