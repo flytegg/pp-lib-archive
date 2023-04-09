b729729ec8500ac36a79e318d6c4165684979065
AkkihBR <contact@akkih.com>
2023-04-09 12:53:17 -0300

    docs: add javadocs to MarketplacePluginManager's methods

--- src/main/kotlin/link/portalbox/pplib/manager/MarketplacePluginManager.kt
+++ src/main/kotlin/link/portalbox/pplib/manager/MarketplacePluginManager.kt
@@ -8,11 +8,25 @@ import link.portalbox.pplib.type.PluginService
 object MarketplacePluginManager {
     private val services: MutableMap<MarketplaceService, PluginService> = mutableMapOf()
 
+    /**
+     * Gets a MarketplacePlugin object for the specified plugin ID from the specified MarketplaceService.
+     *
+     * @param service the MarketplaceService to use for retrieving the plugin
+     * @param id the ID of the plugin to retrieve
+     * @return a MarketplacePlugin object representing the specified plugin
+     * @throws ServiceNotFoundException if the specified service is not found in the services map
+     */
     fun getPlugin(service: MarketplaceService, id: String): MarketplacePlugin {
         val pluginService = services[service]
         return pluginService?.getPlugin(id) ?: throw ServiceNotFoundException()
     }
 
+    /**
+     * Registers a MarketplaceService implementation with the corresponding PluginService.
+     *
+     * @param service the MarketplaceService to register
+     * @param pluginService the PluginService implementation to use for the specified service
+     */
     fun registerService(service: MarketplaceService, pluginService: PluginService) {
         services[service] = pluginService
     }
